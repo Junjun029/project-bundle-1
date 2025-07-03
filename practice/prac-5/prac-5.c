a3cb931e72561259769036ca1fdf70c6df86c02d
Junjun029 <Junaamaja@gmail.com>
2025-07-03 20:55:21 +0700

    prac-5

--- practice/prac-5/prac-5.c
+++ practice/prac-5/prac-5.c
@@ -5,8 +5,22 @@
 
 int main() {
     int input_value = 0;
+    int sum = 0;
     scanf("%d", &input_value);
-    for(int j = 0;);
+    for(int j = 1;j <= input_value/2;j++) {
+        if (input_value%j==0) {
+            sum += j;
+            printf("%d ", j);
+        }
+    }
+    
+    printf("\nsum: %d\n", sum);
+
+    if (sum == input_value) {
+        printf("%d is a P E R F E C T number!\n", input_value);
+    }else {
+        printf("Not a perfect number, sorry %d.\n", input_value);
+    }
 
     return 0;
 }
