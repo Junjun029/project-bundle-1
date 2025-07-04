1398d3fd2d5e47e438412ac50d309db05a3ca295
Junjun029 <Junaamaja@gmail.com>
2025-07-04 23:42:20 +0700

    bruh

--- practice/prac-11/main.c
+++ practice/prac-11/main.c
@@ -1,2 +1,25 @@
 /* prime number checker
 */
+#include <stdio.h>
+
+int main() {
+    int input_value = 0;
+    int prime = 0;
+    scanf("%d", &input_value);
+
+    for(int j = 1;j<=input_value;j++) {
+        if((input_value%j)==0 && ((j != 1) && (j != input_value))) {
+            prime = 0;
+            break;
+        }
+        prime = 1;
+    }
+
+    if (prime) {
+        printf("%d is prime\n", input_value);
+    }else {
+        printf("%d is not prime\n", input_value);
+    }
+
+    return 0;
+}
