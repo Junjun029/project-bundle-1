2dcefe40aed866b560fac18bf078dee03ae540a9
Junjun029 <Junaamaja@gmail.com>
2025-07-04 23:47:09 +0700

    bruh

--- practice/prac-11/main.c
+++ practice/prac-11/main.c
@@ -2,18 +2,23 @@
 */
 #include <stdio.h>
 
+int isPrime(int n) {
+
+    for(int j = 1;j<=n;j++) {
+        if((n%j)==0 && ((j != 1) && (j != n))) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
 int main() {
     int input_value = 0;
-    int prime = 0;
+    
     scanf("%d", &input_value);
 
-    for(int j = 1;j<=input_value;j++) {
-        if((input_value%j)==0 && ((j != 1) && (j != input_value))) {
-            prime = 0;
-            break;
-        }
-        prime = 1;
-    }
+    int prime = isPrime(input_value);
 
     if (prime) {
         printf("%d is prime\n", input_value);
