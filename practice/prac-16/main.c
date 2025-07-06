be41a735057b6059532db2815a7d2cc21bd3a9c8
Junjun029 <Junaamaja@gmail.com>
2025-07-06 12:49:41 +0700

    GCD done for prac-16

--- practice/prac-16/main.c
+++ practice/prac-16/main.c
@@ -3,23 +3,25 @@
 int main() {
     int a = 0, b = 0;
 
+    int temp = 0;
+    
     printf("a: ");
     scanf("%d", &a);
     printf("b: ");
     scanf("%d", &b);
-    
+
     int max, min;
     max = (a - b) > 0 ? a : b;
     min = (a - b) < 0 ? a : b;
 
     // gcd
-    for (int i = 1; i <= min; i++) {
-        printf("%d ", i);
-        if (a % i == 0 && b % i == 0){
-            printf("gcd: %d\n", i);
-            break;
-        }
+    for(int i = 1;i <= min;i++) {
+       if (min%i == 0 && max%i == 0) {
+            temp = i;
+       }
     }
+
+    printf("gcd: %d\n", temp);
     
     return 0;
 }
