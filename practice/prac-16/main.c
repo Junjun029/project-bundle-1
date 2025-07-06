87cea3a44d6718db157b5038b49a6941a5d62daf
Junjun029 <Junaamaja@gmail.com>
2025-07-06 13:30:27 +0700

    tidied the code even more

--- practice/prac-16/main.c
+++ practice/prac-16/main.c
@@ -10,13 +10,12 @@ int main() {
     printf("b: ");
     scanf("%d", &b);
 
-    int max, min;
-    max = (a - b) > 0 ? a : b;
+    int min = 0;
     min = (a - b) < 0 ? a : b;
     
     // gcd
     for(int i = 1;i <= min;i++) {
-       if (min%i == 0 && max%i == 0) {
+       if (a%i == 0 && b%i == 0) {
             gcd = i;
             printf("%d ", gcd);
        }
@@ -25,7 +24,7 @@ int main() {
     printf("\ngcd: %d\n", gcd);
 
     // lcm
-    int lcm  = (min/gcd)*max;
+    int lcm  = (a/gcd)*b;
     printf("lcm: %d\n", lcm);
     
     return 0;
