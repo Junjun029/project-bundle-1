0bbcfc7e4a398f9e9023cd4abbe5425764447c94
Junjun029 <Junaamaja@gmail.com>
2025-07-09 19:37:11 +0700

    HOLY SHIT I MADE IT I MADE TIC TAC TOE HOLY SHITE!!!!!!!!!

--- practice/prac-28/main.c
+++ practice/prac-28/main.c
@@ -8,6 +8,7 @@
 void displayGridDefault();
 void displayGrid(int game[][3]);
 void play(int game[][3]);
+int checkWin(int game[][3], int player);
 
 int main() {
     int game[3][3] = {{-1,-1,-1},
@@ -15,10 +16,38 @@ int main() {
                       {-1,-1,-1}
     };
 
-    displayGridDefault();
     play(game);
 
     return 0;
+}
+int checkWin(int game[][3], int player) {
+    int result = 0;
+    if (game[0][0] == game[1][1] && game[1][1] == game[2][2] && game[2][2] == player) {
+        result = 1;
+    }
+    if (game[0][2] == game[1][1] && game[1][1] == game[2][0] && game[2][0] == player) {
+        result = 1;
+    }
+    if (game[0][0] == game[0][1] && game[0][1] == game[0][2] && game[0][2] == player) {
+        result = 1;
+    }
+    if (game[1][0] == game[1][1] && game[1][1] == game[1][2] && game[1][2] == player) {
+        result = 1;
+    }
+    if (game[2][0] == game[2][1] && game[2][1] == game[2][2] && game[2][2] == player) {
+        result = 1;
+    }
+    if (game[0][0] == game[1][0] && game[1][0] == game[2][0] && game[2][0] == player) {
+        result = 1;
+    }
+    if (game[0][1] == game[1][1] && game[1][1] == game[2][1] && game[2][1] == player) {
+        result = 1;
+    }
+    if (game[0][2] == game[1][2] && game[1][2] == game[2][2] && game[2][2] == player) {
+        result = 1;
+    }
+
+    return result;
 }
 void play(int game[][3]) {
     int player = 1;
@@ -26,7 +55,22 @@ void play(int game[][3]) {
     int full = 0;
 
     while(1) {
+        system("clear");
+        printf("\n============================================================\n");
+        displayGridDefault();
+        printf("\n============================================================\n");
+        displayGrid(game);
+        if(checkWin(game, 1)) {
+            printf("Player %d wins!", 1);
+            break;
+        }
+        if(checkWin(game, 0)) {
+            printf("Player %d wins!", 2);
+            break;
+        }
+        
         if (full == 9) {
+            
             printf("Game draw!\n"); 
             break;
         }
@@ -100,7 +144,6 @@ void play(int game[][3]) {
                 printf("It's 1-9 dumbass\n");
                 continue; 
         }
-        displayGrid(game);
         if(player || !player) {
             player = !player;
             full++;
@@ -109,7 +152,9 @@ void play(int game[][3]) {
 }
 void displayGridDefault() {
     for(int i = 1;i<=9;i++) {
-        printf("\t%d\t!", i);
+        printf("\t%d\t", i);
+        if(i%3!=0) 
+            printf("!");
         if(i%3==0) {
             printf("\n");
         }
@@ -120,13 +165,22 @@ void displayGrid(int game[][3]) {
     for(int i = 0;i<3;i++) {
         for(int j = 0;j<3;j++) {
             if(game[i][j] == 1) {
-                printf("\tX\t!");
+                printf("\tX\t");
+                if(j!=2) {
+                    printf("!");
+                }
             }
             if(game[i][j] == 0) {
-                printf("\tO\t!");
+                printf("\tO\t");
+                if(j!=2) {
+                    printf("!");
+                }
             }
             if(game[i][j] == -1) {
-                printf("\t \t!");
+                printf("\t \t");
+                if(j!=2) {
+                    printf("!");
+                }
             }
         }
         printf("\n");
